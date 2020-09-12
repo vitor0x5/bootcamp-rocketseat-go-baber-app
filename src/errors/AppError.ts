4c119de914f020f66833b0d8a8091377affea045
vitor0x5 <vitorhugo.glh@gmail.com>
2020-09-12 17:09:38 -0300

    Adding the AppError class for general errors

---
+++ src/errors/AppError.ts
@@ -0,0 +1,12 @@
+class AppError {
+    public readonly messsage: string;
+
+    public readonly statusCode: number;
+    
+    constructor(message: string, statusCode = 400) {
+      this.messsage = message;
+      this.statusCode = statusCode;
+    }
+}
+
+export default AppError;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -3,6 +3,7 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import Appointment from '../models/Appointment';
+import AppError from '../errors/AppError'
 
 interface Request {
   provider_id: string;
@@ -16,7 +17,7 @@ class CreateAppointmentService {
     const appointmentDate = startOfHour(date);
 
     if (await appointmentsRepository.findByDate(appointmentDate)) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({
