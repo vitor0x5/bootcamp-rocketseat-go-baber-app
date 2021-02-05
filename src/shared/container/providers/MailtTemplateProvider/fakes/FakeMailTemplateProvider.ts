1db36ada4eab57f7b49d0aced93aaa308ba0bf3d
vitor0x5 <vitorhugo.glh@gmail.com>
2021-02-04 21:50:16 -0300

    Finish email provider implementation

--- src/shared/container/providers/MailtTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailtTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,12 +1,8 @@
 import IMailTemplateProvider from '@shared/container/providers/MailtTemplateProvider/models/IMailTemplateProvider';
-import IParseMailTemplateDto from '@shared/container/providers/MailtTemplateProvider/dtos/IParseMailTemplateDto';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({
-    template,
-    variables,
-  }: IParseMailTemplateDto): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
--- src/shared/container/providers/MailtTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailtTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -2,13 +2,18 @@ import handlebars from 'handlebars';
 
 import IMailTemplateProvider from '@shared/container/providers/MailtTemplateProvider/models/IMailTemplateProvider';
 import IParseMailTemplateDto from '@shared/container/providers/MailtTemplateProvider/dtos/IParseMailTemplateDto';
+import * as fs from 'fs';
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDto): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
     return parseTemplate(variables);
   }
 }
