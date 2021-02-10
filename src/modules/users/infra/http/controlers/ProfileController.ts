2b98eb02d448dbf5c4392a94c5eccdba74c4d4d8
vitor0x5 <vitorhugo.glh@gmail.com>
2021-02-09 22:08:13 -0300

    Finish update profile service

--- src/modules/users/infra/http/controlers/ProfileController.ts
+++ src/modules/users/infra/http/controlers/ProfileController.ts
@@ -1,10 +1,17 @@
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // exibição de perfil
+    const user_id = request.user.id;
+
+    const showProfile = container.resolve(ShowProfileService);
+
+    const user = await showProfile.execute({ user_id });
+
+    return response.json(user);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
