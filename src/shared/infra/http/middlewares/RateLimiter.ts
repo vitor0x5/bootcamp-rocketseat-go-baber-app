046766e837ef19319f9641a0d19247f8bcc835e7
vitor0x5 <vitorhugo.glh@gmail.com>
2021-04-06 22:37:51 -0300

    Implementing request rate limit (against DDoS)

---
+++ src/shared/infra/http/middlewares/RateLimiter.ts
@@ -0,0 +1,28 @@
+import { Response, Request, NextFunction } from 'express';
+import Redis from 'ioredis';
+import cacheConfig from '@config/cache';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = new Redis(cacheConfig.config.redis);
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
